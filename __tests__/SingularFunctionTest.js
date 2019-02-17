5db71cf2b0a383728017e0420c72804d3c32f782
Mike Claros <shibachurro@gmail.com>
2019-02-16 22:54:51 -0800

    added test folder, and jest library

---
+++ __tests__/SingularFunctionTest.js
@@ -0,0 +1,18 @@
+/// TODO figure out how to save json in a variable without return a pending promise
+/// TODO search through node-fetch doc 
+
+const nbaUtils = require('../util/nbaUtils');
+const nbaData = require('../api/nbaData');
+
+let firstName = "Quinn";
+let lastName = "Cook";
+
+
+
+
+
+
+
+
+
+

616cc749a6075daf518a6773d51af14520e86296
Laturine <gbarbosa333@gmail.com>
2019-02-26 22:26:06 -0800

    Add standard.js, Defined globals in package.json
    Suppress npm warning with lint command, Apply standard to new functions

--- api/yahooData.js
+++ api/yahooData.js
@@ -1,95 +1,92 @@
-const { URLSearchParams } = require('url');
-const fetch = require('node-fetch');
-
+const { URLSearchParams } = require('url')
+const fetch = require('node-fetch')
 
 /**
  * @param {string} authCode code
  * @return {Promise<pending>} access_token and refresh_token
  */
-initAccessToken = function(authCode) {
+const initAccessToken = function (authCode) {
+  const params = new URLSearchParams({
+    redirect_uri: 'oob',
+    code: authCode,
+    grant_type: 'authorization_code'
+  })
 
-    const params = new URLSearchParams({
-        redirect_uri: "oob",
-        code: authCode,
-        grant_type: "authorization_code"
+  return fetch('https://api.login.yahoo.com/oauth2/get_token', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + process.env.YAHOO_API_KEY,
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: params
+  })
+    .then(res => {
+      return res.json()
     })
-
-    return fetch('https://api.login.yahoo.com/oauth2/get_token', {
-        method: 'POST',
-        headers: {
-            "Authorization": "Basic " + process.env.YAHOO_API_KEY,
-            "content-type": "application/x-www-form-urlencoded"
-        },
-        body: params
-        })
-        .then(res => {
-            return res.json();
-        })
-        .then(response => {
-            console.log(response);
-            return response;
-        })
-        .catch(err => {
-            console.log(`${err} Promise error`)
-        })
+    .then(response => {
+      console.log(response)
+      return response
+    })
+    .catch(err => {
+      console.log(`${err} Promise error`)
+    })
 }
 
 /**
  * @param {string} refreshToken refresh_token
  * @return {Promise<pending>} access_token and refresh_token
  */
-getAccessToken = function(refreshToken) {
+const getAccessToken = function (refreshToken) {
+  const params = new URLSearchParams({
+    redirect_uri: 'oob',
+    refresh_token: refreshToken,
+    grant_type: 'refresh_token'
+  })
 
-    const params = new URLSearchParams({
-        redirect_uri: 'oob',
-        refresh_token: refreshToken,
-        grant_type: 'refresh_token'
-    });
-
-    return fetch('https://api.login.yahoo.com/oauth2/get_token',options = {
-        method:'POST',
-        headers: {
-            'Authorization': "Basic " + process.env.YAHOO_API_KEY,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: params
-    })
+  return fetch('https://api.login.yahoo.com/oauth2/get_token', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + process.env.YAHOO_API_KEY,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: params
+  })
     .then(res => {
-        return res.json();
+      return res.json()
     })
     .then(response => {
-        console.log(response.refresh_token);
-        return response;
+      console.log(response.refresh_token)
+      return response
     })
     .catch(err => {
-        console.log(`${err} Promise error`);
+      console.log(`${err} Promise error`)
     })
 }
 
 /**
  * @return {Promise<pending>} Yahoo Auth code
  */
-getYahooAuthCode = function() {
+const getYahooAuthCode = function () {
 // TODO future selenium method to login to yahoo and grab authCode
 
-    const redirectURL = "oob";
-    const responseType = "code";
+  const redirectURL = 'oob'
+  const responseType = 'code'
 
-    const params = "?" + "client_id=" + process.env.YAHOO_CLIENT_ID + "&" + "redirect_uri=" + redirectURL + "&" + "response_type=" + responseType;
+  const params = '?' + 'client_id=' + process.env.YAHOO_CLIENT_ID + '&' + 'redirect_uri=' + redirectURL + '&' + 'response_type=' + responseType
 
-    return fetch('https://api.login.yahoo.com/oauth2/request_auth' + params)
-        .then(res => {
-            return res;
-        })
-        .then(response => {
-            console.log(response.url);
-            return response;
-        })
-        .catch(err => {
-            console.log(`${err}`)
-        })
+  return fetch('https://api.login.yahoo.com/oauth2/request_auth' + params)
+    .then(res => {
+      return res
+    })
+    .then(response => {
+      console.log(response.url)
+      return response
+    })
+    .catch(err => {
+      console.log(`${err}`)
+    })
 }
 
-module.exports.getAccessToken =  getAccessToken;
-module.exports.getYahooAuthCode = getYahooAuthCode;
-module.exports.initAccessToken = initAccessToken;
+module.exports.getAccessToken = getAccessToken
+module.exports.getYahooAuthCode = getYahooAuthCode
+module.exports.initAccessToken = initAccessToken
--- app.js
+++ app.js
@@ -1,37 +1,33 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 const nbaUtil = require('./util/nbaUtils.js')
 const nbaData = require('./api/nbaData.js')
-const yahooData = require('./api/yahooData')
+// const yahooData = require('./api/yahooData')
 
+const client = new Discord.Client()
 
-const client = new Discord.Client();
+client.on('message', message => {
+  if (message.content === 'hi') {
+    message.channel.send('hellow world.')
+  }
 
-client.on("message", message => {
-	
-	if (message.content === 'hi') {
-		
-		message.channel.send('hellow world.');
-		
-	}
+  if (message.content === 'days') {
+    message.channel.send(nbaUtil.getDaysOfWeek(nbaUtil.getCurrentMonday()))
+  }
 
-	if (message.content === 'days') {
-		message.channel.send(nbaUtil.getDaysOfWeek(nbaUtil.getCurrentMonday()));
-	}
-
-	if (message.content === 'player') {
-		let firstName = "Quinn";
-		let lastName = "Cook";
-		nbaData.getPlayers().then(
-			resp =>{
-				let teamId = nbaUtil.getTeamId(resp, firstName, lastName);
-				nbaData.getTeamSchedule(teamId).then(resp => console.log(resp));
-			}
-		);
-	}
+  if (message.content === 'player') {
+    let firstName = 'Quinn'
+    let lastName = 'Cook'
+    nbaData.getPlayers().then(
+      resp => {
+        let teamId = nbaUtil.getTeamId(resp, firstName, lastName)
+        nbaData.getTeamSchedule(teamId).then(resp => console.log(resp))
+      }
+    )
+  }
 })
 
 // console.log(yahooData.getYahooAuthCode().then(resp => console.log(resp)));
 // console.log(yahooData.initAccessToken('').then(resp => console.log(resp)));
 // console.log(yahooData.getAccessToken('').then(resp => console.log(resp)));
 
-client.login(process.env.DISCORD_TOKEN);  
+client.login(process.env.DISCORD_TOKEN)

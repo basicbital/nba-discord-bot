3595bc6baa645afb13bc8986ea3c835cf2fb2718
Joseph Pena <josephapena91@gmail.com>
2019-06-26 23:09:48 -0700

    -added next/last week function -modified createRosterDateMap to accept offset -changed 'next' to 'previous' -Added changes to account for roster checking on Sundays -changed Test to reflect this Sunday roster checking change

--- commands/previous.js
+++ commands/previous.js
@@ -2,10 +2,11 @@ const nbaUtil = require('../util/nbaUtils')
 const yUtils = require('../util/yahooUtils')
 
 module.exports = {
-  name: 'roster',
-  description: 'returns user roster for current week',
+  name: 'previous',
+  description: 'returns user roster for previous of current week',
   async execute (message, args) {
-    let userRoster = await yUtils.createRosterDateMap(1).then(resp => (resp))
+    let userRoster = await yUtils.createRosterDateMap(-6).then(resp => (resp))
+    //-6 to account for default Sunday start and -6 to get to last monday
     let msg = ''
     if (userRoster !== null) {
       msg = nbaUtil.displayUserMap(userRoster)
